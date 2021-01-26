f18eea19bc513cf7eefafe062a75430a17c804ef
Shwetang <shwetangsingh5@gmail.com>
2021-01-26 15:31:01 +0530

    material-button update

--- src/Components/CreateNote.jsx
+++ src/Components/CreateNote.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
 const CreateNote = () => {
     return ( 
         <>
             <div className="main_note">
                 <form>
-                    <input type="text" placeholder="Title" />
-                    <textarea name="" placeholder="Write a note..." cols="30" rows="10"></textarea>
+                    <input type="text" placeholder="Title" autoComplete="off" />
+                    <textarea name="" placeholder="Write a note..." cols="" rows=""></textarea>
+                    <Button>
+                        <AddIcon className="plus_sign" />
+                    </Button>
                 </form>
             </div>
         </>
