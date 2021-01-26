b10fec8e9ee069d7d2f33d7cddcc7e813d9bdbbe
Shwetang <shwetangsingh5@gmail.com>
2021-01-26 12:27:55 +0530

    footer added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 ReactDOM.render(
   <React.Fragment>
     <Header />
+    <Footer />
   </React.Fragment>,
   document.getElementById('root')
 );
