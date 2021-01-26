10cf7b40353b6fbd156a4122507cab8a7bfdc1f2
Shwetang <shwetangsingh5@gmail.com>
2021-01-26 11:49:07 +0530

    header component added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Components/Header';
 
 ReactDOM.render(
-    <App />,
+  <React.Fragment>
+    <Header />
+    <App />
+  </React.Fragment>,
   document.getElementById('root')
 );
 
