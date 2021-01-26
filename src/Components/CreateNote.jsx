b81c007476d39f68aeb89ab89b63e9384dfc546f
Shwetang <shwetangsingh5@gmail.com>
2021-01-26 18:23:18 +0530

    expand on click update

--- src/Components/CreateNote.jsx
+++ src/Components/CreateNote.jsx
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
 const CreateNote = (props) => {
+    let [expand, setExpand] = useState(false);
     let [note, setNote] = useState({
         title: "",
         content: ""
@@ -32,11 +33,21 @@ const CreateNote = (props) => {
         <>
             <div className="main_note">
                 <form>
-                    <input onChange={handleChange} type="text" value={note.title} name="title" placeholder="Title" autoComplete="off" />
-                    <textarea onChange={handleChange} value={note.content} name="content" placeholder="Write a note..." cols="" rows=""></textarea>
-                    <Button onClick={handleAdd}>
-                        <AddIcon className="plus_sign" />
-                    </Button>
+                    {
+                        expand
+                        &&
+                        <input onChange={handleChange} type="text" value={note.title} name="title" placeholder="Title" autoComplete="off" />
+                    }
+                    
+                    <textarea onChange={handleChange} onClick={() => setExpand(true)} onDoubleClick={() => setExpand(false)} value={note.content} name="content" placeholder="Write a note..." cols="" rows="" />
+                    
+                    {
+                        expand
+                        &&
+                        <Button onClick={handleAdd}>
+                            <AddIcon className="plus_sign" />
+                        </Button>
+                    }
                 </form>
             </div>
         </>
