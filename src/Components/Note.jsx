21bdb890f64c09c073156d5405ab922797c221f9
Shwetang <shwetangsingh5@gmail.com>
2021-03-03 23:12:24 +0530

    developer name added

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,3 +1,7 @@
+
+//   Develped by - Shwetang
+//   GitHub profile - https://github.com/Shwetang550 
+
 import React from 'react';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
